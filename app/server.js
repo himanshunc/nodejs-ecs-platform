6c1e48b25c703ba48565b6a31a8b5a4a02eb482d
Himanshu Chaudhari <himanshunchaudhari@gmail.com>
2026-04-10 00:53:56 +0530

    Update app response for CI/CD verification

--- app/server.js
+++ app/server.js
@@ -3,5 +3,5 @@ const app = require("./src/app");
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}`);
+  console.log(`Himanshu Your App running on port ${PORT}`);
 });
--- app/src/app.js
+++ app/src/app.js
@@ -7,7 +7,7 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
   res.status(200).json({
-    message: "Node.js ECS app is running"
+    message: "Node.js ECS app is running - CI/CD verified"
   });
 });
 
